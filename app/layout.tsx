1b4c2943a8ec07efee8c141e78c1011687f34155
HaiderAziz428 <kryptochaingames@gmail.com>
2025-11-07 20:47:19 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "FYP",
   description:
-    "",
+    "Final Year Project",
 };
 
 export default function RootLayout({
